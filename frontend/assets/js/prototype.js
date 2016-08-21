a555bcced0660c35d11bee28e11f9a3045fa2480
Alexandru <alex@bitriver.de>
2016-08-21 11:53:40 +0200

    prototype login modal

--- frontend/assets/js/prototype.js
+++ frontend/assets/js/prototype.js
@@ -7,3 +7,9 @@ $('.glyphicon-heart').on('click', function(event) {
     $(event.currentTarget).addClass('red');
 });
 
+$('.loginmodal-submit').on('click', function (event) {
+    setTimeout(function () {
+        $('#loginModal').modal('hide');
+        $('#btn-login').text('Potato');
+    }, 1500);
+});
