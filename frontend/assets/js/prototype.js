c5f14e0f23abbd40a66b353f582701c2b637d197
Alexandru <alex@bitriver.de>
2016-08-21 11:16:13 +0200

    made some changes for prototyping

---
+++ frontend/assets/js/prototype.js
@@ -0,0 +1,9 @@
+$('.glyphicon-floppy-disk').on('click', function(event) {
+    
+});
+
+$('.glyphicon-heart').on('click', function(event) {
+    $(event.currentTarget).removeClass('black');
+    $(event.currentTarget).addClass('red');
+});
+
